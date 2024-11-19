137b4db6e4f90f0bba180f7a3e66f4140ff4ecdc
student <student@Admins-MacBook-Pro.local>
2024-11-19 17:48:28 -0500

    First commit

---
+++ rps.py
@@ -0,0 +1,37 @@
+import time
+
+Rock = "Rock"
+Paper = "Paper"
+Scissors = "Scissors"
+Player1 = ""
+Player2 = ""
+
+
+print("Welcome to Rock Paper Scissors!")
+time.sleep(2.0)
+print("Would you learn about the game?")
+YN = input("")
+# fill this in
+if YN == "Yes":
+    print("placeholder explanation")
+elif YN == "No":
+    ("Okay, it seems like you have the hang of it")
+time.sleep(2.0)
+players = input("")
+if players == "1":
+    print("You need 2 players to play")
+else:
+    print("Let's play!")
+time.sleep(1.0)
+print("Pick either rock, paper, or scissors.")
+time.sleep(1.0)
+player1Choice = input("")
+player2Choice = input("")
+if player1Choice == "Rock":
+    
+
+
+
+
+
+
